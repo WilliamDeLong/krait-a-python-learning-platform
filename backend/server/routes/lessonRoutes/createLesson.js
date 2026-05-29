b484b834e20e70a7a48979db33d130ff0eece121
WilliamDeLong <w_delong@salemstate.edu>
2026-05-29 15:58:56 -0400

    Comments and whatnot

--- backend/server/routes/lessonRoutes/createLesson.js
+++ backend/server/routes/lessonRoutes/createLesson.js
@@ -7,29 +7,30 @@ const data_default = { title: "Placeholder title.", instructions: "You know how 
 
 router.post('/create', async (req, res) => {
     const data_2_check = req.body
-    //console.log(typeof(data_2_check));
-    //console.log(typeof(data_default));
+    // I need all of the elements in an object so I can iterate through them
     
-    //check if a lesson with the same title already exists
+    //Due to lessons having default values, we need to set empty values to their defaults so that the system can check for duplicates accurately.
     Object.keys(data_default).forEach(element => {
         if (data_2_check[element] === undefined) 
             data_2_check[element] = data_default[element];
         //console.log(`Testing ${element}:\n\tDefault Value: ${data_default[element]}\n\tInputted Value: ${data_2_check[element]}\n\tComparison: ${data_default[element]===data_2_check[element]}`)
     });
+    
+    // After we have our defaulted sets, we can now properly set them to their individual variables and check each one for duplicates.
     const { title, instructions, default_script, solution_verification, is_test, chapter_no, order_within_chapter, documentation_id } = data_2_check
+
+    
     const lesson_title = await lessonSchema.findOne({ title: title })
     if (lesson_title)
         return res.status(409).send({ message: "There is already a lesson with that title, please select another." })
+    
     const lesson_order_within_chapter = await lessonSchema.findOne({chapter_no: chapter_no, order_within_chapter: order_within_chapter})
     //console.log(chapter_no);
     if (lesson_order_within_chapter)
         return res.status(409).send({ message: "There's a pre-existing lesson with that order value in the same chapter, please select another." })
 
-    //generates the hash
     
-    //parse the generated hash into the password
-    
-    //creates a new user
+    //creates the new lesson
     const createLesson = new lessonSchema({
         title: title,
         instructions: instructions,
